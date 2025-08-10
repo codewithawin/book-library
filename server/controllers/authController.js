8b8acbe6a7dfb1cf8bb094b244a737cf078bb290
codewithawin <narwinatienza@gmail.com>
2025-08-10 21:24:43 +0800

    Create sign in and sign out routes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 import User from "../models/userModel.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -27,6 +28,43 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).send("User has been created!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || email === "" || !password || password === "") {
+    next(errorHandler(400, "All fields are required!"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      next(errorHandler(404, "User not found!"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      next(errorHandler(400, "Invalid password!"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...info } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(info);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signout = (req, res, next) => {
+  try {
+    res.clearCookie("access_token");
+    res.status(200).json({ message: "User has been logged out!" });
   } catch (error) {
     next(error);
   }
