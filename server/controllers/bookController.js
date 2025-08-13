2ee8b6722cbc46608494d5c5c17d1e7d48ba1409
codewithawin <narwinatienza@gmail.com>
2025-08-13 23:11:42 +0800

    Create update and delete book routes

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -40,3 +40,43 @@ export const getBookById = async (req, res, next) => {
     next(error);
   }
 };
+
+export const updateBook = async (req, res, next) => {
+  const { title, author, genre, yearPublished, description } = req.body;
+
+  try {
+    const book = await Book.findOne({
+      _id: req.params.id,
+      userId: req.user.id,
+    });
+    if (!book) {
+      return next(errorHandler(404, "Book not found!"));
+    }
+
+    book.title = title ?? book.title;
+    book.author = author ?? book.author;
+    book.genre = genre ?? book.genre;
+    book.yearPublished = yearPublished ?? book.yearPublished;
+    book.description = description ?? book.description;
+
+    const updatedBook = await book.save();
+    res.json(updatedBook);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteBook = async (req, res, next) => {
+  try {
+    const book = await Book.findOneAndDelete({
+      _id: req.params.id,
+      userId: req.user.id,
+    });
+    if (!book) {
+      return next(errorHandler(404, "Book not found!"));
+    }
+    res.json({ message: "Book deleted!" });
+  } catch (error) {
+    next(error);
+  }
+};
--- server/routes/bookRoute.js
+++ server/routes/bookRoute.js
@@ -1,8 +1,10 @@
 import express from "express";
 import {
   createBook,
+  deleteBook,
   getBookById,
   getBooks,
+  updateBook,
 } from "../controllers/bookController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -11,5 +13,7 @@ const router = express.Router();
 router.post("/", verifyToken, createBook);
 router.get("/", verifyToken, getBooks);
 router.get("/:id", verifyToken, getBookById);
+router.put("/:id", verifyToken, updateBook);
+router.delete("/:id", verifyToken, deleteBook);
 
 export default router;
