b7d5ebdc5f865318a83a451ee6f56d744fbbb6c0
codewithawin <narwinatienza@gmail.com>
2025-08-13 22:16:30 +0800

    Create get book routes

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -1,10 +1,41 @@
 import Book from "../models/bookModel.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createBook = async (req, res, next) => {
   try {
     const newBook = new Book({ userId: req.user.id, ...req.body });
     const savedBook = await newBook.save();
     res.status(201).json(savedBook);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getBooks = async (req, res, next) => {
+  const search = req.query.search || "";
+  const regex = new RegExp(search, "i");
+
+  try {
+    const books = await Book.find({
+      userId: req.user.id,
+      $or: [{ title: regex }, { author: regex }, { genre: regex }],
+    }).sort({ createdAt: -1 });
+    res.json(books);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getBookById = async (req, res, next) => {
+  try {
+    const book = await Book.findOne({
+      _id: req.params.id,
+      userId: req.user.id,
+    });
+    if (!book) {
+      return next(errorHandler(404, "Book not found!"));
+    }
+    res.json(book);
   } catch (error) {
     next(error);
   }
--- server/routes/bookRoute.js
+++ server/routes/bookRoute.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { createBook } from "../controllers/bookController.js";
+import {
+  createBook,
+  getBookById,
+  getBooks,
+} from "../controllers/bookController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/", verifyToken, createBook);
+router.get("/", verifyToken, getBooks);
+router.get("/:id", verifyToken, getBookById);
 
 export default router;
