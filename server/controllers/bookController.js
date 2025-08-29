c032ec54f545b02037f7bbc60a9f92483ae869e4
codewithawin <narwinatienza@gmail.com>
2025-08-29 23:44:26 +0800

    Update api book controller and model

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -18,7 +18,12 @@ export const getBooks = async (req, res, next) => {
   try {
     const books = await Book.find({
       userId: req.user.id,
-      $or: [{ title: regex }, { author: regex }, { genre: regex }],
+      $or: [
+        { title: regex },
+        { author: regex },
+        { genre: regex },
+        { isbn: regex },
+      ],
     }).sort({ createdAt: -1 });
     res.json(books);
   } catch (error) {
@@ -32,9 +37,7 @@ export const getBookById = async (req, res, next) => {
       _id: req.params.id,
       userId: req.user.id,
     });
-    if (!book) {
-      return next(errorHandler(404, "Book not found!"));
-    }
+    if (!book) return next(errorHandler(404, "Book not found!"));
     res.json(book);
   } catch (error) {
     next(error);
@@ -42,22 +45,32 @@ export const getBookById = async (req, res, next) => {
 };
 
 export const updateBook = async (req, res, next) => {
-  const { title, author, genre, yearPublished, description } = req.body;
+  const {
+    title,
+    author,
+    genre,
+    publishedYear,
+    isbn,
+    description,
+    coverImage,
+    status,
+  } = req.body;
 
   try {
     const book = await Book.findOne({
       _id: req.params.id,
       userId: req.user.id,
     });
-    if (!book) {
-      return next(errorHandler(404, "Book not found!"));
-    }
+    if (!book) return next(errorHandler(404, "Book not found!"));
 
     book.title = title ?? book.title;
     book.author = author ?? book.author;
     book.genre = genre ?? book.genre;
-    book.yearPublished = yearPublished ?? book.yearPublished;
+    book.publishedYear = publishedYear ?? book.publishedYear;
+    book.isbn = isbn ?? book.isbn;
     book.description = description ?? book.description;
+    book.coverImage = coverImage ?? book.coverImage;
+    book.status = status ?? book.status;
 
     const updatedBook = await book.save();
     res.json(updatedBook);
@@ -72,9 +85,7 @@ export const deleteBook = async (req, res, next) => {
       _id: req.params.id,
       userId: req.user.id,
     });
-    if (!book) {
-      return next(errorHandler(404, "Book not found!"));
-    }
+    if (!book) return next(errorHandler(404, "Book not found!"));
     res.json({ message: "Book deleted!" });
   } catch (error) {
     next(error);
--- server/models/bookModel.js
+++ server/models/bookModel.js
@@ -7,15 +7,43 @@ const bookSchema = new mongoose.Schema(
       ref: "User",
       required: true,
     },
-    title: { type: String, required: true, trim: true },
-    author: { type: String, required: true },
-    genre: { type: String },
-    yearPublished: { type: Number },
-    description: { type: String },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    genre: {
+      type: String,
+      required: true,
+    },
+    publishedYear: {
+      type: Number,
+    },
+    isbn: {
+      type: String,
+      unique: true,
+      sparse: true,
+    },
+    description: {
+      type: String,
+    },
+    coverImage: {
+      type: String,
+      default:
+        "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg",
+    },
+    status: {
+      type: String,
+      enum: ["available", "borrowed", "lost"],
+      default: "available",
+    },
   },
   { timestamps: true }
 );
 
-const Book = new mongoose.model("Book", bookSchema);
-
+const Book = mongoose.model("Book", bookSchema);
 export default Book;
